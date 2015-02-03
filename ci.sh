adfca9aff502b2fb27b2ade08982d4976ea2728c
Daniel Standage <daniel.standage@gmail.com>
2015-02-02 23:40:41 -0500

    Oops Pdom data not publicly available yet

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@
 # https://github.com/BrendelGroup/HymHub/blob/master/LICENSE.
 set -eo pipefail
 
-for spec in Ador Aflo Amel Bimp Bter Cflo Dmel Hsal Mrot Nvit Pdom Sinv Tcas
+for spec in Ador Aflo Amel Bimp Bter Cflo Dmel Hsal Mrot Nvit Sinv Tcas
 do
   bash species/${spec}/data.sh -w species/${spec} -d -f -c
 done
