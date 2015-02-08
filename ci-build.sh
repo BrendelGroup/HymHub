f225d07698ecf1a9056af8c0d08cf4c410382efd
Daniel Standage <daniel.standage@gmail.com>
2015-02-07 20:12:21 -0500

    Updated build scripts

--- ci-build.sh
+++ ci-build.sh
@@ -5,7 +5,17 @@
 # https://github.com/BrendelGroup/HymHub/blob/master/LICENSE.
 set -eo pipefail
 
-for spec in Ador Aflo Amel Bimp Bter Cflo Dmel Hsal Mrot Nvit Sinv Tcas
+for spec in Ador Aflo Amel Bimp Bter Cflo Dmel Hsal Mrot Nvit Pdom Sinv Tcas
 do
   bash species/${spec}/data.sh -w species/${spec} -d -f -t -s -c
 done
+
+for feattype in iloci genereps mrnas cds
+do
+  cp species/Ador/${feattype}.tsv data/${feattype}.tsv
+  for spec in Ador Aflo Amel Bimp Bter Cflo Dmel Hsal Mrot Nvit Pdom Sinv Tcas
+  do
+    tail -n +2 species/${spec}/${feattype}.tsv >> data/${feattype}.tsv
+  done
+done
+shasum -c data/checksums.sha
