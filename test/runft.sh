0885ab8124c0b811692b5ba9df9e60f14c708afa
Daniel Standage <daniel.standage@gmail.com>
2015-02-09 15:51:54 -0500

    Added exons functional test

--- test/runft.sh
+++ test/runft.sh
@@ -10,9 +10,15 @@ python scripts/feature-desc.py \
     --iloci test/ftest-iloci.gff3 test/ftest-iloci.fa test/iloci-test.tsv \
     --gnreps test/ftest-genereps.gff3 test/ftest-genereps.fa test/genereps-test.tsv \
     --mrnas test/ftest-mrnas.gff3 test/ftest-mrnas.fa test/mrnas-test.tsv \
-    --cds test/ftest-cds.gff3 test/ftest-cds.fa test/cds-test.tsv
+    --cds test/ftest-cds.gff3 test/ftest-cds.fa test/cds-test.tsv \
+    --exons test/ftest-exons.gff3 test/ftest-exons.fa test/exons-pre-test.tsv
 
-for ftype in iloci genereps mrnas cds
+head -n 1 test/exons-pre-test.tsv > test/exons-test.tsv
+grep 'NT_033778.3_19722259-19722625' test/exons-pre-test.tsv >> test/exons-test.tsv
+grep 'NW_003798164.1_151172-151385' test/exons-pre-test.tsv >> test/exons-test.tsv
+grep 'PdomSCFr1.2-0001_325761-325941' test/exons-pre-test.tsv >> test/exons-test.tsv
+
+for ftype in iloci genereps mrnas cds exons
 do
   echo -n "[HymHub]     $ftype functional test..."
   diff -q test/ftest-${ftype}.tsv test/${ftype}-test.tsv
