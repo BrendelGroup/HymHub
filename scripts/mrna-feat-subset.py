135edae7b2b7bb8ddb479e93e033342efd5e0366
Daniel Standage <daniel.standage@gmail.com>
2015-08-05 09:53:38 -0400

    Improved script for pulling out subsets of feature tables [skip ci]

--- scripts/mrna-feat-subset.py
+++ scripts/mrna-feat-subset.py
@@ -11,12 +11,24 @@ import sys
 
 desc = 'Extract feature info for hiLocus mRNA reps'
 parser = argparse.ArgumentParser(description=desc)
-parser.add_argument('--hiloci', type=argparse.FileType('r'),
-                    default='data/hilocus-reps.tsv',
-                    help='hiLocus mRNA rep data table')
 parser.add_argument('-o', '--outfile', type=argparse.FileType('w'),
                     metavar='FILE', default=sys.stdout,
                     help='write results to specified file')
+parser.add_argument('-s', metavar='int', type=int, default=3,
+                    help='column in hiLocus file containing species label; '
+                    'default is 3')
+parser.add_argument('-m', metavar='int', type=int, default=5,
+                    help='column in hiLocus file containing mRNA ID; default '
+                    'is 5')
+parser.add_argument('-S', metavar='int', type=int, default=1,
+                    help='column in feature file containing species label; '
+                    'default is 1')
+parser.add_argument('-M', metavar='int', type=int, default=3,
+                    help='column in feature file containing mRNA ID; default '
+                    'is 3')
+parser.add_argument('hiloci', type=argparse.FileType('r'),
+                    default='data/hilocus-all.tsv',
+                    help='hiLocus mRNA data table')
 parser.add_argument('featfile', type=argparse.FileType('r'),
                     help='mRNA feature data table')
 args = parser.parse_args()
@@ -25,8 +37,8 @@ mrnas2keep = dict()
 next(args.hiloci)
 for line in args.hiloci:
     values = line.split('\t')
-    species = values[0]
-    mrnaid = values[1]
+    species = values[args.s - 1]
+    mrnaid = values[args.m - 1]
     if species not in mrnas2keep:
         mrnas2keep[species] = dict()
     mrnas2keep[species][mrnaid] = True
@@ -34,7 +46,7 @@ for line in args.hiloci:
 print str(next(args.featfile)).rstrip()
 for line in args.featfile:
     values = line.split('\t')
-    species = values[0]
-    mrnaid = values[2]
+    species = values[args.S - 1]
+    mrnaid = values[args.M - 1]
     if species in mrnas2keep and mrnaid in mrnas2keep[species]:
         print line.rstrip()
